e1ada83c65c98e57c5971f59faed708bf07de553
Youssef Mashaly <metwally.y9@gmail.com>
2025-04-13 03:11:32 -0400

    flask setup

--- popup.js
+++ popup.js
@@ -1,1 +1,9 @@
-console.log("This is a popup!")
+console.log("This is a popup!") //console log to test
+
+fetch('http://127.0.0.1:5000/run-python-script', {  // flask data request
+  method: 'POST'
+})
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('output').textContent = data.output;  })    // making flask data into an id element to display in html file
+  .catch(error => console.error('Fetch error:', error));
